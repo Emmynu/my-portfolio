ea83facc6f362af59462f68e9ee0b04e588936d8
Emmanuel <oketunbi.olufunke@gmail.com>
2024-12-11 15:22:11 -0800

    aos animation at  about section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { createBrowserRouter, createRoutesFromElements,Route, RouterProvider } from "react-router-dom";
 import NavigationBar from "./components/navigation-bar";
 import Main from "./components/main";
-import AOS from "aos"
-import "aos/dist/aos.css"
-import { useEffect } from "react";
-
 
 
 const router = createBrowserRouter(createRoutesFromElements(
@@ -14,9 +10,6 @@ const router = createBrowserRouter(createRoutesFromElements(
 ))
 
 export default function App() {
-  useEffect(()=>{
-    AOS.init()
-  },[])
     return <>
     <title>My Portfolio</title>
     <RouterProvider router={router}></RouterProvider>
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,17 +1,22 @@
-import { Link } from "react-router-dom"
+import AOS from "aos"
+import "aos/dist/aos.css"
 import "../styles/about.css"
+import { useEffect } from "react"
 
 
 export default function About() {
-    return <main className="about-container">
-            <section>
+
+    useEffect(()=>{
+        AOS.init()
+    },[])
+    return <main className="about-container ">
+            <section data-aos={"zoom-in-down"} data-aos-duration={900}>
                 <img src="https://react-vite-proj-18-tailwind-portfolio.netlify.app/assets/about-03f57d6e.svg" alt="about-me" />
             </section>
             <section>
                 <header>
-                    <h2>About Me</h2>
-                    <div className="line"></div>
-                    <p className="">Passionate Frontend Developer with expertise in React, Next.js, MongoDB,Tailwind CSS, Typescript and alot more. Adept at translating design concepts into responsive and visually appealing web applications. Proven track record of actively contributing to multiple projects, showcasing strong problem-solving skills. Eager to leverage my skills and drive for excellence in a dynamic professional environment. </p>
+                    <h2 data-aos={"zoom-in-up"} data-aos-duration={900}>About Me</h2>
+                    <p data-aos={"fade-up"} data-aos-duration={1000}>Passionate Frontend Developer with expertise in React, Next.js, MongoDB,Tailwind CSS, Typescript and alot more. Adept at translating design concepts into responsive and visually appealing web applications. Proven track record of actively contributing to multiple projects, showcasing strong problem-solving skills. Eager to leverage my skills and drive for excellence in a dynamic professional environment. </p>
                 </header>
             </section>
         </main>
