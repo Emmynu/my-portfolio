94f1d8705603ffaa4e10e8ffcb07d3c1013ef055
Emmanuel <oketunbi.olufunke@gmail.com>
2024-12-12 23:03:11 -0800

    project section added

---
+++ src/components/contact.jsx
@@ -0,0 +1,15 @@
+import AOS from "aos"
+import "aos/dist/aos.css"
+import "../styles/contact.css"
+
+export default function Contact() {
+    return <main className="contact-container">
+        <section>
+            <h3>Get In Touch</h3>
+        </section>
+        <section>
+            <div></div>
+            <div></div>
+        </section>
+    </main>
+}
